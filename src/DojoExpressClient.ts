4b964e81c8f6729f9c62b8120225e0bc4fe2e890
Tone <tone@kizmet.org>
2023-10-23 10:00:54 -0400

    dojo.destroy ProvenTxReq.tst

--- src/DojoExpressClient.ts
+++ src/DojoExpressClient.ts
@@ -56,6 +56,10 @@ export class DojoExpressClient implements DojoClientApi {
     this.authrite = options?.authrite
   }
 
+  async destroy(): Promise<void> {
+    // Nothing to destroy
+  }
+
   //
   // HTTP API FUNCTIONS
   //
